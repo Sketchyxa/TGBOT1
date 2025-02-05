0ef3aacd0a6aef72981c6f368c945571fdede10e
Siriusatop123 <152073541+Sketchyxa@users.noreply.github.com>
2025-02-05 21:21:01 +0100

    Add files via upload

---
+++ configs/settings.py
@@ -0,0 +1,7 @@
+token = '7202612373:AAFZWNIKLWarRGG-DqQuFofD2gbQxHOlGkQ'
+dir = r'C:\\'
+version = '1.0'
+admins_id = [1257601441]
+
+standart_welcome = "Приветствуем вас в беседе!"
+tg_channel = 'https://t.me/Siriusatop228'
